1a21b9fe6feb4427f128ac60b3096720d77cbd9f
DanilaColun <colun.danila@gmail.com>
2026-04-23 13:37:58 +0300

    modified logger.js added input validation

--- src/logger.js
+++ src/logger.js
@@ -1,6 +1,10 @@
 function createLogger(appName) {
+  if (typeof appName !== "string" || appName.trim() === "") {
+    throw new Error("app name needs to be a string and not empty");
+  }
+
   return function log(message) {
-    console.log(`[${appName}] ${message}`);
+    console.log(`[${appName}] ${String(message)}`);
   };
 }
 

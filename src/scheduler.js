aa675e6df79a2695b3b7eff153b53f9aeed4e4bd
DanilaColun <colun.danila@gmail.com>
2026-04-23 13:40:18 +0300

    modified scheduler.js added use of our logger.js and added input validation

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,11 +1,28 @@
-console.log("scheduler.js started");
+const config = require("./config");
+const createLogger = require("./logger");
+
+const log = createLogger(config.appName);
+
+log("scheduler.js started");
 
 function scheduleTask(name, interval, task) {
+  if (typeof name !== "string" || name.trim() === "") {
+    throw new Error("task name must be a string and not empty");
+  }
+
+  if (typeof interval !== "number" || interval <= 0) {
+    throw new Error("interval needs to be positive number");
+  }
+
+  if (typeof task !== "function") {
+    throw new Error("task needs to be a function");
+  }
+
   setInterval(task, interval);
 }
 
-scheduleTask("running task", 10000, () => {
-  console.log("running");
+scheduleTask("running task", 10000, function () {
+  log("running");
 });
 
 module.exports = scheduleTask;

61c245aa338761f342c1a8ee1b330ee8d02607ea
DanilaColun <colun.danila@gmail.com>
2026-05-19 21:37:56 +0300

    used scheduler task errors

--- src/scheduler/scheduleTask.js
+++ src/scheduler/scheduleTask.js
@@ -1,9 +1,27 @@
+const SchedulerError = require("../errors/SchedulerError");
 const validateTaskOptions = require("../validators/taskValidator");
 
-function scheduleTask(name, interval, task) {
+function scheduleTask(name, interval, task, options = {}) {
   validateTaskOptions(name, interval, task);
 
-  const intervalId = setInterval(task, interval);
+  const onError = typeof options.onError === "function" ? options.onError : null;
+
+  const intervalId = setInterval(async function () {
+    try {
+      await task();
+    } catch (error) {
+      const schedulerError = new SchedulerError("Scheduled task failed", {
+        context: {
+          taskName: name,
+          originalErrorMessage: error.message
+        }
+      });
+
+      if (onError) {
+        onError(schedulerError);
+      }
+    }
+  }, interval);
 
   return intervalId;
 }
--- src/scheduler/startScheduler.js
+++ src/scheduler/startScheduler.js
@@ -1,13 +1,31 @@
+const SchedulerError = require("../errors/SchedulerError");
 const scheduleTask = require("./scheduleTask");
 
-function startScheduler(dependencies) {
+function startScheduler(dependencies = {}) {
   const logger = dependencies.logger;
+
+  if (!logger || typeof logger.log !== "function") {
+    throw new SchedulerError("Logger dependency is needed", {
+      context: {
+        dependency: "logger"
+      }
+    });
+  }
 
   logger.log("scheduler started");
 
-  return scheduleTask("background task", 10000, function () {
-    logger.log("background task done");
-  });
+  return scheduleTask(
+    "background task",
+    10000,
+    async function () {
+      logger.log("background task done");
+    },
+    {
+      onError(error) {
+        logger.log(`${error.name}: ${error.message}`);
+      }
+    }
+  );
 }
 
 module.exports = startScheduler;

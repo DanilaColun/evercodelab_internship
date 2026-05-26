bc1e979c1d4d9da3c67315972e02193ca4498f47
DanilaColun <colun.danila@gmail.com>
2026-05-26 18:33:37 +0300

    added start of server from index with npm start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const appConfig = require("./config/appConfig");
 const Logger = require("./logger/logger");
+const createApp = require("./http/createApp");
 
 const logger = new Logger(appConfig.appName, {
-  level: appConfig.logLevel
+  level: appConfig.logLevel,
 });
 
-logger.info("app started");
+const app = createApp({
+  logger,
+});
+
+app.listen(appConfig.port, () => {
+  logger.info(`app started on port ${appConfig.port}`);
+});

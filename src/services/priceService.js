9c8908a5cf8c4776064610afabc65a1ebecac5e4
DanilaColun <colun.danila@gmail.com>
2026-05-27 23:52:34 +0300

    added binance service with retry and price service

---
+++ src/services/priceService.js
@@ -0,0 +1,46 @@
+const NotFoundError = require("../errors/NotFoundError");
+
+class PriceService {
+  constructor(dependencies = {}) {
+    const { currencyRepository, binanceService } = dependencies;
+
+    if (!currencyRepository) {
+      throw new Error("Currency repository is required");
+    }
+
+    if (!binanceService) {
+      throw new Error("Binance service is required");
+    }
+
+    this.currencyRepository = currencyRepository;
+    this.binanceService = binanceService;
+  }
+
+  async getPricesByCurrency(currency, options = {}) {
+    const currencyFromDb = this.currencyRepository.findByTicker(currency);
+
+    if (!currencyFromDb) {
+      throw new NotFoundError("Currency not found", {
+        requestId: options.requestId,
+        context: {
+          currency
+        }
+      });
+    }
+
+    const prices = await this.binanceService.getAllPrices({
+      requestId: options.requestId
+    });
+
+    const filteredPrices = prices.filter((price) => {
+      return price.symbol.includes(currency);
+    });
+
+    return {
+      currency,
+      prices: filteredPrices
+    };
+  }
+}
+
+module.exports = PriceService;

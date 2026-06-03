3bf573e0b0ec9917d0d45c90c850b07b037fdd7e
DanilaColun <colun.danila@gmail.com>
2026-06-03 14:55:31 +0300

    fixed load database config from environment

--- src/database/databaseConfig.js
+++ src/database/databaseConfig.js
@@ -1,7 +1,10 @@
 const path = require("path");
 
+require("dotenv").config();
+
 const databaseConfig = {
-  filename: process.env.DATABASE_FILE || path.join(process.cwd(), "data", "app.sqlite"),
+  filename:
+    process.env.DATABASE_FILE || path.join(process.cwd(), "data", "app.sqlite"),
 };
 
 module.exports = databaseConfig;
